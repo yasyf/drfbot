837fcf107a2e0112f081b7fa15ee00af10302f0c
Yasyf Mohamedali <yasyfm@gmail.com>
2016-10-03 02:04:30 -0400

    Cut down on company mention spam.

--- src/interactions/company_mention.js
+++ src/interactions/company_mention.js
@@ -12,6 +12,16 @@ import util from '../util';
 const MENTION_THRESHOLD = 60; // minutes
 const MIN_NAME_LENGTH = 3;
 
+function checkSurroundingTokens(match: string, input: string, index: number) {
+  if (index != 0 && input[index - 1] != ' ') {
+    let endIndex = index + match.length;
+    if (endIndex != input.length - 1 && input[endIndex + 1] != ' ') {
+      return false;
+    }
+  }
+  return true;
+}
+
 export default class CompanyMentionInteraction extends BaseInteraction {
   patterns = api.getCompanyPatterns();
   messageTypes = ['ambient'];
@@ -19,8 +29,12 @@ export default class CompanyMentionInteraction extends BaseInteraction {
   hook(bot: SlackBot, message: Message) {
     const handleMentions = (company, mentions) => {
       if (
-        company.name.length < MIN_NAME_LENGTH
-        || dictionary.contains(company.name)
+        !message.entities.company
+        && (
+          company.name.length < MIN_NAME_LENGTH
+          || !checkSurroundingTokens(message.match[0], message.match.input, message.match.index)
+          || (dictionary.contains(company.name) && company.name == company.name.toLowerCase())
+        )
       ) {
         return;
       }
@@ -57,6 +71,6 @@ export default class CompanyMentionInteraction extends BaseInteraction {
         }
       });
     };
-    api.getCompany(message.match[0]).then(handleCompany);
+    api.getCompany(message.entities.company || message.match[0]).then(handleCompany);
   }
 }
